f906a0200897a62c5737bfc33b91f1506a7347fc
Alexander Popov <Alexander.Popov@regium.com>
2018-04-19 11:54:11 +0300

    add logic count

--- src/Counter.js
+++ src/Counter.js
@@ -3,23 +3,45 @@ import './App.css';
 
 import Store from './Store';
 
-const initialState = { count: 0 }
+const initialState = {count: 0}
+
+function updateState(state, action) {
+  switch (action.type) {
+    case 'INC':
+      return {count: state.count + action.amount}
+    case 'DEC':
+      return {count: state.count - action.amount}
+    case 'RES':
+      return {count: 0}
+    default:
+      return state
+  }
+}
+
+const incAction = {type: 'INC', amount: 1};
+const decAction = {type: 'DEC', amount: 1};
+const resAction = {type: 'RES'};
+
+const store = new Store(updateState, initialState);
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
 
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1,
+  componentDidMount() {
+    store.subscribe(() => {
+      this.forceUpdate();
     });
   }
 
+  reset = () => {
+    store.update(resAction)
+  }
+
+  increment = () => {
+    store.update(incAction)
+  }
+
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
+    store.update(decAction)
   }
 
   handleChange = () => {
@@ -29,8 +51,9 @@ class Counter extends Component {
   render() {
     return (
       <div className="App">
-        <h1>{this.state.count}</h1>
+        <h1>{store.state.count}</h1>
         <button onClick={this.increment}>+</button>
+        <button onClick={this.reset}>reset</button>
         <button onClick={this.decrement}>-</button>
         <br/>
         <h2>Amount</h2>
